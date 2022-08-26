ed33faa11487f8cf34bea8a5a9c5878a00e74352
Brendan McShane <brendanmcs7@gmail.com>
2022-08-26 11:21:38 -0400

    Remove demo code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import logo from "./logo.svg";
 import "./App.css";
-import myimg from "./myimg.png";
 
 export function App() {
 
@@ -8,7 +7,6 @@ export function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <img src={myimg} alt="test_image" />
         <p style={{ "fontFamily": '"Work Sans"' }}>hello world</p>
       </header>
     </div>
