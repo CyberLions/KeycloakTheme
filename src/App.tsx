d1d32475d8a3fa5511aa05c4003d80dd8774b302
Brendan McShane <brendanmcs7@gmail.com>
2022-08-26 11:28:37 -0400

    Update logo and branding

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ export function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p style={{ "fontFamily": '"Work Sans"' }}>hello world</p>
       </header>
     </div>
   );
