37f69ef97166f5be95b132a162f251944bd64e24
Brendan McShane <brendanmcs7@gmail.com>
2022-08-26 16:09:18 -0400

    Revert "remove missing ref"
    This reverts commit adf2ac577bf8189bd3f83dd80e48b58e1988ff40.

--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,7 @@ import {
 import { useCssAndCx } from "tss-react";
 import tos_en_url from "./tos_en.md";
 import tos_fr_url from "./tos_fr.md";
+import "./kcMessagesExtension"
 
 const { kcContext } = getKcContext({
   /* Uncomment to test th<e login page for development */
